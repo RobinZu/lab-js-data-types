945ba0ad9858067939effe52348a70ffb9fc1dca
Robin Zürn <schreibrobin@yahoo.de>
2024-07-17 17:45:17 +0200

    done

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ const s2 = "fed";
 const s3 = "Ted";
 const s4 = "bread";
 const s5 = "and";
+let total = "";
 
-console.log("1");
-console.log("Hello");
+total += s1 +''+ s2 + ''+s3+''+s4+''+s5
+console.log(total)
+
 
 
 // Concatenate the string variables into one new string
@@ -24,6 +26,11 @@ console.log("Hello");
 *******************************************/
 const part1 = "java";
 const part2 = "script";
+let partOne = part1.cameLtaiL;
+let partTwo = part2.cameLtaiL;
+
+result = partOne + partTwo;
+console.log(result);
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
 
@@ -36,7 +43,7 @@ const part2 = "script";
 /*******************************************
     Iteration 2.1 | Calculate Tip
 *******************************************/
-const billTotal = 84;
+
 
 // Calculate the tip (15% of the bill total)
 
@@ -54,7 +61,12 @@ const billTotal = 84;
 
 
 // Print the generated random number
+let billTotal = 84;
 
+tipAmount = billTotal / 100 * 15;
+console.log(tipAmount);
+console.log( billTotal+ tipAmount)
+           
 
 
 /*******************************************
@@ -66,15 +78,29 @@ const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
 const expression1 = a && b;
+// tip ------false
+ console.log(expression1);
 
 const expression2 = a || b;
+// tip ------true
+console.log(expression2);
 
 const expression3 = !a && b;
+// tip ------true--- false and false is false??????????, because it unequals true?
+console.log(expression3);
 
 const expression4 = !(a && b);
+// tip ------false
+ console.log(expression3);
 
 const expression5 = !a || !b;
+// tip ------false??????????, because it unequals true?
+// console.log(expression);
 
 const expression6 = !(a || b);
+// tip ------true------false??????????, because it unequals true?
+console.log(expression6);
 
 const expression7 = a && a;
+// tip ------true, because it unequals true?
+(expression6);
